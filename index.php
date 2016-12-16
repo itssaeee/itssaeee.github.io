a40119ea89c6bfe38c6ac88205271d3e37513d68
root <root@localhost.localdomain>
2016-12-16 02:06:21 -0500

    UPLOAD MO

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Syril's Page</title>
+<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<link rel="stylesheet" type="text/css" href="../css/534.css" />
+</head>
+<body>
+    <div id="wrapper">
+        <div id="headerwrap">
+        <div id="header">
+        <p><center>Hello and welcome to my page. My name is Syril Daren Lopez</center</p>
+        </div>
+        </div>
+        <div id="contentwrap">
+        <div id="content">
+	<p><center>             The worlds not perfect, but its there for us trying the best it can. Thats what makes it so damn beautiful.             </center></p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
